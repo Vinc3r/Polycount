2c3fb6b8eebeaa7ed256c1f4f798443d4fa0cba8
V!nc3r <Vinc3r@users.noreply.github.com>
2017-03-26 20:35:35 +0200

    Create nothing-is-3D-tools.py

---
+++ nothing-is-3D-tools.py
@@ -0,0 +1,84 @@
+bl_info = {
+    "name": "Nothing-is-3D tools",
+    "description": "Some scripts 3D realtime workflow oriented",
+    "author": "Vincent Lamy",
+    "location": "3D view toolshelf - Addons tab",
+}
+
+import bpy
+
+global C
+C = bpy.context
+global D
+D = bpy.data
+
+class renameUVChannel(bpy.types.Operator):
+    """Normalize UV channel naming"""
+    bl_idname = "nothing3d.rename_uv_channel"
+    bl_label = "Rename UV chans as UVMap and UV2"
+    
+    def execute(self, context):
+        for obj in C.selected_objects:
+            if obj.type == 'MESH':
+                try:
+                    obj.data.uv_textures[0].name = "UVMap"
+                    try:
+                        obj.data.uv_textures[1].name = "UV2"
+                    except:
+                        print("no UV2 on "+str(obj.name))
+                        pass
+                except:
+                    print("no UV on "+str(obj.name))
+                    pass
+        return {'FINISHED'}
+
+class SelectUVChannel(bpy.types.Operator):
+    """Select desired UV channel"""
+    bl_idname = "nothing3d.select_uv_channel"
+    bl_label = "Select desired UV channel"
+    select_UV = bpy.props.IntProperty()
+    
+    def execute(self, context):
+        for obj in C.selected_objects:
+            if obj.type == 'MESH':
+                try:
+                    obj.data.uv_textures[self.select_UV].active = True
+                except:
+                    print("no UV2 on "+str(obj.name))
+                    pass
+        return {'FINISHED'}
+    
+class Nothing3DPanel(bpy.types.Panel):
+    bl_label = "Nothing-is-3D tools"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Addons"
+
+    def draw(self, context):
+        layout = self.layout  
+        scene = context.scene
+        
+        box1 = layout.box()
+        box1.label(text = "UV channels")
+        
+        row = box1.row(align = True)
+        row.label(text = "Select ")
+        row.operator("nothing3d.select_uv_channel", text = "UV1").select_UV = 0
+        row.operator("nothing3d.select_uv_channel", text = "UV2").select_UV = 1
+        
+        row = box1.row()
+        row.operator("nothing3d.rename_uv_channel", text = "Normalize naming")
+        
+
+def register():
+    bpy.utils.register_class(renameUVChannel)
+    bpy.utils.register_class(SelectUVChannel)
+    bpy.utils.register_class(Nothing3DPanel)
+
+def unregister():
+    bpy.utils.unregister_class(renameUVChannel)
+    bpy.utils.unregister_class(SelectUVChannel)
+    bpy.utils.unregister_class(Nothing3DPanel)
+
+if __name__ == "__main__":
+    register()
