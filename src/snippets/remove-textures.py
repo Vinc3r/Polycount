20032d274736298abca510cc10cb0f0dc364a915
V!nc3r <Vinc3r@users.noreply.github.com>
2017-11-21 10:47:50 +0100

    force image unlink

--- src/snippets/remove-textures.py
+++ src/snippets/remove-textures.py
@@ -8,25 +8,26 @@ D = bpy.data
 for img in D.images:
     if ".emit" in img.name:
         print("removed file emit "+str(img.name))
-        D.images.remove(img)
+        # delete image, unlink it
+        D.images.remove(img, True)
         
     elif ".LM" in img.name:
         print("removed file LM "+str(img.name))
-        D.images.remove(img)
+        D.images.remove(img, True)
         
     elif "AO_" in img.name:
         print("removed file AO "+str(img.name))
-        D.images.remove(img)
+        D.images.remove(img, True)
 
 for img in D.textures:
     if ".emit" in img.name:
         print("removed texture emit "+str(img.name))
-        D.textures.remove(img)
+        D.textures.remove(img, True)
         
     elif ".LM" in img.name:
         print("removed texture LM "+str(img.name))
-        D.textures.remove(img)
+        D.textures.remove(img, True)
         
     elif "AO_" in img.name:
         print("removed texture AO "+str(img.name))
-        D.textures.remove(img)
+        D.textures.remove(img, True)
