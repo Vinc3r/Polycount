eefc740385c94956ed8f8d4a35cb5545eb845793
V!nc3r <Vinc3r@users.noreply.github.com>
2017-11-24 12:01:16 +0100

    preparing for pattern detection
    here default is "AO_", like when using baketool plugin with automatic texture assignment

--- src/snippets/remove-textures.py
+++ src/snippets/remove-textures.py
@@ -1,33 +1,16 @@
 import bpy
 import os
 
-# yep, that's ugly code
-
 D = bpy.data
 
+pattern = "AO_"
+
 for img in D.images:
-    if ".emit" in img.name:
-        print("removed file emit "+str(img.name))
-        # delete image, unlink it
-        D.images.remove(img, True)
-        
-    elif ".LM" in img.name:
-        print("removed file LM "+str(img.name))
-        D.images.remove(img, True)
-        
-    elif "AO_" in img.name:
+    if pattern in img.name:
         print("removed file AO "+str(img.name))
         D.images.remove(img, True)
 
 for img in D.textures:
-    if ".emit" in img.name:
-        print("removed texture emit "+str(img.name))
-        D.textures.remove(img, True)
-        
-    elif ".LM" in img.name:
-        print("removed texture LM "+str(img.name))
-        D.textures.remove(img, True)
-        
-    elif "AO_" in img.name:
+    if pattern in img.name:
         print("removed texture AO "+str(img.name))
         D.textures.remove(img, True)
