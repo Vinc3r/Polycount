9056b824571aa328c9eebf7f075191faa7fc1776
vinc3r <Vinc3r@users.noreply.github.com>
2017-11-24 20:26:04 +0100

    simplification of code new category new panels in tools detection of current render engine for materials tools

---
+++ src/nothing-is-3D-tools.py
@@ -0,0 +1,196 @@
+bl_info = {
+    "name": "Nothing-is-3D tools",
+    "description": "Some scripts 3D realtime workflow oriented",
+    "author": "Vincent (Vinc3r) Lamy",
+    "location": "3D view toolshelf - Nthg is 3D tab",
+    "category": "Mesh",	
+    "wiki_url": 'https://github.com/Vinc3r/BlenderScripts',
+    "tracker_url": 'https://github.com/Vinc3r/BlenderScripts/issues',
+    "version": (2017, 11, 24, 2020),
+}
+
+import bpy
+
+class renameUVChannel(bpy.types.Operator):
+    """Rename the first two UV channels as UVMap and UV2"""
+    bl_idname = "nothing3d.rename_uv_channel"
+    bl_label = "Rename UV channels"
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH' and len(obj.data.uv_textures) > 0 :
+                obj.data.uv_textures[0].name = "UVMap"
+                if obj.data.uv_textures[1] is not None :
+                    obj.data.uv_textures[1].name = "UV2"
+        return {'FINISHED'}
+
+class SelectUVChannel(bpy.types.Operator):
+    """Select the desired UV channel"""
+    bl_idname = "nothing3d.select_uv_channel"
+    bl_label = "Select the desired UV channel"
+    select_UV = bpy.props.IntProperty()
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH' and len(obj.data.uv_textures) > 0 and obj.data.uv_textures[self.select_UV] is not None:
+                obj.data.uv_textures[self.select_UV].active = True
+        return {'FINISHED'}
+
+class BImtlSetIntensity(bpy.types.Operator):
+    """Set intensity value to 1"""
+    bl_idname = "nothing3d.bi_mtl_set_intensity"
+    bl_label = "Set intensity value to 1"
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH' and len(obj.data.materials) > 0:
+                for mat in obj.data.materials:
+                    mat.diffuse_intensity = 1
+        return {'FINISHED'}
+    
+class BImtlSetWhite(bpy.types.Operator):
+    """Set diffuse color to white"""
+    bl_idname = "nothing3d.bi_mtl_set_white"
+    bl_label = "Set diffuse color to white"
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH' and len(obj.data.materials) > 0 :
+                for mat in obj.data.materials:
+                    mat.diffuse_color = (1,1,1)
+        return {'FINISHED'}
+    
+class BImtlResetAlpha(bpy.types.Operator):
+    """Reset transparency parameters"""
+    bl_idname = "nothing3d.bi_mtl_reset_alpha"
+    bl_label = "Reset transparency parameters"
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH' and len(obj.data.materials) > 0 :
+                for mat in obj.data.materials:
+                    mat.transparency_method = 'Z_TRANSPARENCY'
+                    mat.alpha = 1
+                    mat.use_transparency = False
+        return {'FINISHED'}
+    
+class disableAutosmooth(bpy.types.Operator):
+    """Disable mesh Auto Smoothing"""
+    bl_idname = "nothing3d.disable_auto_smooth"
+    bl_label = "Disable mesh Auto Smoothing"
+    
+    def execute(self, context):
+        for obj in context.selected_objects:
+            if obj.type == 'MESH':
+                obj.data.use_auto_smooth = False
+                #obj.modifier_add(type='EDGE_SPLIT')
+                #obj.modifiers["EdgeSplit"].split_angle = 1.48353
+        return {'FINISHED'}    
+
+class Nthg3DMeshPanel(bpy.types.Panel):
+    bl_label = "Meshes"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Nthg is 3D"
+
+    def draw(self, context):
+        layout = self.layout
+        
+        """ UV Channels box """
+
+        UVchanBox = layout.box()
+        UVchanBox.label(text = "UV channels :")
+        
+        row = UVchanBox.row(align = True)
+        row.label(text = "Select ")
+        row.operator("nothing3d.select_uv_channel", text = "UV1").select_UV = 0
+        row.operator("nothing3d.select_uv_channel", text = "UV2").select_UV = 1
+        
+        row = UVchanBox.row()
+        row.operator("nothing3d.rename_uv_channel", text = "Rename UV channels")
+
+class Nothing3DMaterialPanel(bpy.types.Panel):
+    bl_label = "Materials"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Nthg is 3D"
+
+    def draw(self, context):
+        # Blender Render
+        if bpy.context.scene.render.engine == "BLENDER_RENDER":
+            layout = self.layout     
+            row = layout.row(align = True)
+            row.operator("nothing3d.bi_mtl_set_intensity", text = "Diffuse intensity to 1")
+            row.operator("nothing3d.bi_mtl_set_white", text = "", icon = "SOLID")
+            row.operator("nothing3d.bi_mtl_reset_alpha", text = "", icon = "MATCAP_24")
+        # Cycles
+        elif bpy.context.scene.render.engine == "CYCLES":
+            print("Nothing is 3D : Cycles not yet supported")
+        else:
+            print("Nothing is 3D : Render engine not supported")
+
+class Nthg3DStatsPanel(bpy.types.Panel):
+    bl_label = "Stats"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+    bl_category = "Nthg is 3D"
+
+    def draw(self, context):
+        layout = self.layout
+        
+        """ Selection stats box """
+        # thanks to sambler for some piece of code https://github.com/sambler/addonsByMe/blob/master/mesh_summary.py
+        
+        selectionStatsBox = layout.box()
+        row = selectionStatsBox.row(align = True)
+        row.label(text = "Object")
+        row.label(text = "Verts")
+        row.label(text = "Tris")
+
+        # test only selected meshes
+        selectedMeshes = [o for o in bpy.context.selected_objects if o.type == 'MESH']
+              
+        totalTriInSelection = 0
+        totalVertsInSelection = 0
+
+        for element in selectedMeshes:
+            triCount = 0
+            hasNGon = False
+            for poly in element.data.polygons:
+                # first check if quad
+                if len(poly.vertices) == 4:
+                    triCount += 2
+                # or tri
+                elif len(poly.vertices) == 3:
+                    triCount += 1
+                # or oops, ngon here, alert!
+                else:
+                    triCount += 3
+                    hasNGon = True
+            # adding element stats to total count
+            totalTriInSelection += triCount
+            totalVertsInSelection += len(element.data.vertices)
+            # generate table
+            row = selectionStatsBox.row(align = True)
+            row.label(text = "%s" % (element.name))
+            row.label(text = "%i " % (len(element.data.vertices)))
+            if not hasNGon:
+                row.label(text = "%i" % (triCount))
+            else:
+                # visual indicator if ngon
+                row.label(text = "± %i" % (triCount))
+        # show total stats                
+        selectionStatsBox.row().separator() 
+        row = selectionStatsBox.row(align = True)
+        row.label(text = "TOTAL")
+        row.label(text = "%i" % (totalVertsInSelection))
+        row.label(text = "%i" % (totalTriInSelection))        
+
+def register():
+    bpy.utils.register_module(__name__)
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+
+if __name__ == "__main__":
+    register()
