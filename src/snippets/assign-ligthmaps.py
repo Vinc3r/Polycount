ebace86e010a790440ab7f9934da4b8b25167b1c
V!nc3r <Vinc3r@users.noreply.github.com>
2017-11-24 17:44:48 +0100

    some tweaks
    disable default diffuse assignation
    test if texture already exist

--- src/snippets/assign-ligthmaps.py
+++ src/snippets/assign-ligthmaps.py
@@ -6,7 +6,7 @@ print("++++++")
 # get engine name
 ## print(bpy.context.scene.render.engine)
 
-LIGHTMAP_FOLDER_PATH = "//textures"
+LIGHTMAP_FOLDER_PATH = "//textures\HD\LM\T5"
 PATTERN_FILENAME = "{{OBJ_NAME}}.LM.png"
 
 def assign_lightmap():
@@ -24,14 +24,19 @@ def assign_lightmap():
                 texture_file_path = os.path.join(LIGHTMAP_FOLDER_PATH, LIGHTMAP_FILENAME)
                 texture_file_fullpath = bpy.path.abspath(texture_file_path)
                 if os.path.exists(texture_file_fullpath):
-                    new_image = bpy.data.images.load(texture_file_path, check_existing=False)
+                    new_image = bpy.data.images.load(texture_file_path, check_existing=True)
                     new_image.name = LIGHTMAP_FILENAME
-                    new_tex = bpy.data.textures.new(LIGHTMAP_FILENAME, 'IMAGE')
+                    if bpy.data.textures[LIGHTMAP_FILENAME] is not None:
+                        new_tex = bpy.data.textures[LIGHTMAP_FILENAME]
+                    else:
+                        new_tex = bpy.data.textures.new(LIGHTMAP_FILENAME, 'IMAGE')                   
                     new_tex.name = LIGHTMAP_FILENAME
-                    new_tex.image = new_image
+                    new_tex.image = new_image 
                     slot = mat.texture_slots.add()
+                    slot.use_map_color_diffuse = False
                     slot.use_map_ambient = True
                     slot.ambient_factor = 1.0
-                    slot.texture = new_tex
+                    slot.texture = new_tex                    
+                    slot.uv_layer = obj.data.uv_textures[1].name
 
 assign_lightmap()
