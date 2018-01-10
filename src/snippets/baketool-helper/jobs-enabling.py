fd23efc9f66f40d9bc49c35ac09fa70f8c041bda
V!nc3r <Vinc3r@users.noreply.github.com>
2018-01-10 19:44:56 +0100

    some little add to baketool helper

--- src/snippets/baketool-helper/jobs-enabling.py
+++ src/snippets/baketool-helper/jobs-enabling.py
@@ -1,22 +1,30 @@
 import bpy
 
-startJob = 6
-endJob = 8
+startJob = 0
+endJob = 6
 
 D = bpy.data
 
 print("+++++ BAKETOOL HELPER +++++")
-print(str(len(D.scenes[0].BakeTool_Jobs.Jobs))+" jobs sets")
+print("{} existing jobs".format(len(D.scenes[0].BakeTool_Jobs.Jobs)))
         
 def resetAllBakeToolJobs():
     for job in range(len(D.scenes[0].BakeTool_Jobs.Jobs)):
         D.scenes[0].BakeTool_Jobs.Jobs[job].enabled = False
+    print("all jobs disabled")
     
 def setBakeToolJobsEnable():
-    if endJob < len(D.scenes[0].BakeTool_Jobs.Jobs):
+    if endJob <= len(D.scenes[0].BakeTool_Jobs.Jobs):
         for job in range(startJob, endJob):
             D.scenes[0].BakeTool_Jobs.Jobs[job].enabled = True
+            print("job {} enabled".format(job))
+            
+def setBakeToolJobsSamples():
+    wantedSamples = 2048
+    for job in range(len(D.scenes[0].BakeTool_Jobs.Jobs)):
+        D.scenes[0].BakeTool_Jobs.Jobs[job].job_pass.Pass[0].samples = wantedSamples
+    print("samples set to {}".format(wantedSamples))
 
 resetAllBakeToolJobs()
-
 #setBakeToolJobsEnable()
+#setBakeToolJobsSamples()
