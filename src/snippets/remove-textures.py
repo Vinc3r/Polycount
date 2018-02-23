4bba6c3a2838e89f9670893fc67f18cab8334a48
Vinc3r <Vinc3r@users.noreply.github.com>
2018-02-23 14:01:08 +0100

    trying to debug the assign lightmap script

--- src/snippets/remove-textures.py
+++ src/snippets/remove-textures.py
@@ -3,14 +3,14 @@ import os
 
 D = bpy.data
 
-pattern = "AO_"
+pattern = ".LM"
 
 for img in D.images:
     if pattern in img.name:
-        print("removed file AO "+str(img.name))
+        print("removed file "+str(img.name))
         D.images.remove(img, True)
 
 for img in D.textures:
     if pattern in img.name:
-        print("removed texture AO "+str(img.name))
+        print("removed texture "+str(img.name))
         D.textures.remove(img, True)
