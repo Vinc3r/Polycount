48366d527fe78d93350f1ad25a3e02ae1dcf206f
Vinc3r <Vinc3r@users.noreply.github.com>
2018-03-15 15:01:08 +0100

    better way to view wrong paths

--- src/snippets/texture-quick-replace-path.py
+++ src/snippets/texture-quick-replace-path.py
@@ -8,6 +8,15 @@ D = bpy.data
     In this example we tell Blender to use the A: drive instead of network path.
 """
 
+wrongPath = r"\\my-wrong-path"
+correctPath = r"A:\my-right-path"
+
+print("+++ path correction +++")
+
 for img in D.images:
-    img.filepath = img.filepath.replace(r"\\my-network-path-example", "A:")
-    
+    img.filepath = img.filepath.replace(wrongPath, correctPath)
+
+# check in the console what's rest to fix
+for img in D.images:
+    if not img.filepath.startswith(correctPath):
+        print(img.filepath)
