008a6f9aed344a9504b69ae24f842ce5503f53df
Vinc3r <Vinc3r@users.noreply.github.com>
2018-03-16 16:49:47 +0100

    added some console indications during process

--- src/snippets/delete-custom-normals.py
+++ src/snippets/delete-custom-normals.py
@@ -2,17 +2,19 @@ import bpy
 import math
 
 """
-    This batch custom normal deletion on selected objects, and also activate autosmooth with 85°
+    This batch custom normals deletion on selected objects, and also activate autosmooth with 85°
 """
 
 print("+++++ DELETE Normals Custom Data +++++")
 
 objects = [o for o in bpy.context.selected_objects if o.type == 'MESH']
 
-for obj in objects:
-    mesh = obj.data
+for obj in range(len(objects)):
+    current_object = objects[obj]
+    mesh = current_object.data
     if mesh.has_custom_normals:
-        bpy.context.scene.objects.active = obj
+        print("%i of %i - processing %s" % ((obj+1), len(objects), current_object.name))
+        bpy.context.scene.objects.active = current_object
         bpy.ops.mesh.customdata_custom_splitnormals_clear()
         mesh.use_auto_smooth = True
         mesh.auto_smooth_angle = math.radians(85)
